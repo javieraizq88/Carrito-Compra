1d513a231c65d7dd56c00603c9310886c9fab4ef
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-09-08 13:15:44 -0300

    agregado productos y CSS

--- proyecto/src/views/home.js
+++ proyecto/src/views/home.js
@@ -1,203 +1,111 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import '../App.css';
 import { Context } from "../store/appContext";
+import data from "../data.js";
 
 
-const Chocolate = props => {
-    const { store, actions } = useContext(Context);
-
-    return (
-
-        <div className="card mr-3 col-md-3 col-sm-5 mb-3 ">
-            <div className="card-header">
-                Chocolate amargo
-            </div>
-            <div className="card-body">
-                $ 3.990 x unidad
-            </div>
-
-            <div className="justify content-between">
-                {/* boton para actival del Modal */}
-                <button
-                    type="button"
-                    class="mb-3 mr-4 ml-4 btn btn-secondary"
-                    data-toggle="modal"
-                    data-target="#exampleModal"
-                >
-                    Ver detalles
-                </button>
-
-                <button
-                    type="button"
-                    className="mb-3 btn btn-primary "
-                    name="ChocolateAmargo"
-                    id="ChocolateAmargo"
-                    onClick={(e) => { actions.cambiarCheckbox(e) }}
-                    value={"ChocolateAmargo"}
-                >
-                    Agregar
-                </button>
-            </div>
-
-            {/* Modal */}
-            <div
-                class="modal fade"
-                id="exampleModal"
-                tabindex="-1"
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-            >
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5
-                                class="modal-title"
-                                id="exampleModalLabel">
-                                Chocolate Amargo
-                            </h5>
-                            <button
-                                type="button"
-                                class="close"
-                                data-dismiss="modal"
-                                aria-label="Close"
-                            >
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p><strong>Descripción del producto:</strong>
-                            Barra de chocolate amargo de 300 grs con 72% de cacao</p>
-
-                            <p><strong>Valor:</strong>
-                            $ 3.990 x unidad</p>
-
-                            <img
-                                className="card card-img"
-                                id="card-hoteles2"
-                                src="/img/chocolate.jpg"
-                                alt="imagen chocolate amargo" />
-
-                            <small>Tip: El chocolate amargo disminuye el riesgo cardiovascular</small>
-                        </div>
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                className="mb-3 btn btn-primary "
-                                name="Chocolate"
-                                id="Chocolate"
-                                onClick={(e) => { actions.cambiarCheckbox(e) }}
-                                value={"Chocolate"}
-                            >
-                                Agregar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-        </div>
-    )
-}
-
-const Manzana = props => {
-    const { store, actions } = useContext(Context);
-
-    return (
-        <div className="card mr-3 col-md-3 col-sm-5 mb-3">
-            <div className="card-body">
-                Manzana
-    </div>
-            <button
-                type="button"
-                className="mb-3 btn btn-primary custom-control-button"
-                name="Manzana"
-                id="Manzana"
-                onClick={(e) => { actions.cambiarCheckbox(e) }}
-                value={"Manzana"}
-            >
-                Agregar
-    </button>
-        </div>
-    )
-}
-
-const Pera = props => {
-    const { store, actions } = useContext(Context);
-
-    return (
-        <div className="card mr-3 col-md-3 col-sm-5 mb-3">
-            <div className="card-body">
-                Pera
-    </div>
-            <button
-                type="button"
-                className="mb-3 btn btn-primary custom-control-button"
-                name="Pera"
-                id="Pera"
-                onClick={(e) => { actions.cambiarCheckbox(e) }}
-                value={"Pera"}
-            >
-                Agregar
-    </button>
-        </div>
-    )
-}
 
 const Home = props => {
 
     const { store, actions } = useContext(Context);
 
+    useEffect(() => {
+        fetch('./db.json')
+            .then(response => response.json())
+            .then(data => this.setState({ data: data }));
+    }, [])
+
     return (
         <div className="container">
-            <h1>Agrega lo que quieres comprar</h1>
-            <div className="">
-                <form>
-                    <div className="row row-cols-1 row-cols-md-3">
+            <div className="card-group">
+                <div className="col-md-9">
+                    <h2>Agrega lo que quieres comprar</h2>
+                    <form>
+                        <div className="row row-cols-1 row-cols-md-3">
+                            {data.map(product => {
+                                return (
+                                    <div className="card mr-3 col-md-5 col-sm-5 mb-3 ">
+                                        <div className="card-header">
+                                            {product.product_name}
+                                        </div>
+                                        <div className="card-body">
+                                            <div id="descripcion">
+                                                <p><strong>Descripción del producto:</strong> <br />
+                                                    {product.description}</p>
+                                            </div>
+                                            <div id="valor">
+                                                <p><strong>Valor:</strong> <br />
+                                                    {product.valor}</p>
+                                            </div>
 
-                        <Chocolate />
-                        <Manzana />
-                        <Pera />
+                                            <img
+                                                className="card card-img"
+                                                src={product.imagen}
+                                                alt="imagen" />
+                                        </div>
 
-                    </div>
-                </form>
-            </div>
+                                        <div className="justify content-between">
+                                            <button
+                                                type="button"
+                                                className="mb-3 btn btn-primary "
+                                                name={product.product_value}
+                                                id={product.product_value}
+                                                onClick={(e) => { actions.cambiarCheckbox(e) }}
+                                                value={product.product_value}
+                                            >
+                                                Agregar
+                                        </button>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                            }
 
-            <hr />
+                        </div>
+                    </form>
+                </div>
 
-            <div className="inferior">
-                <h1>Lo que compraste</h1>
-                <div
-                    className="card"
-                    id="selection">
-                    {
-                        store.product.Manzana !== "Manzana" &&
-                            store.product.Pera !== "Pera" &&
-                            store.product.Chocolate !== "Chocolate"
-                            ?
-                            <alert id="alert" className="alert alert-danger" role="alert"
-                            >No haz elegido nada</alert>
-                            :
-                            <div id="add">
-                                <ul>
-                                    <ol>
-                                        {store.product.Chocolate}
-                                    </ol>
-                                    <ol>
-                                        {store.product.Manzana}
-                                    </ol>
-                                    <ol>
-                                        {store.product.Pera}
-                                    </ol>
-                                </ul>
-                            </div>
-                    }
+                <hr />
 
-                </div> {/* fin renderizado */}
-            </div>
+                <div className="col-md-3 " id="compras">
+                    <h3>Tu compra</h3>
+                    <div
+                        className="card"
+                        id="selection">
+                        {
+                            store.product.Manzana !== "Manzana" &&
+                                store.product.Pera !== "Pera" &&
+                                store.product.Chocolate !== "Chocolate" &&
+                                store.product.Porsche !== "Porsche" &&
+                                store.product.IPhone !== "IPhone"
+                                ?
+                                <alert id="alert" className="alert alert-danger" role="alert"
+                                >No haz elegido ningun producto</alert>
+                                :
+                                <div id="add">
+                                    <ul>
+                                        <ol>
+                                            {store.product.Chocolate}
+                                        </ol>
+                                        <ol>
+                                            {store.product.Manzana}
+                                        </ol>
+                                        <ol>
+                                            {store.product.Pera}
+                                        </ol>
+                                        <ol>
+                                            {store.product.Porsche}
+                                        </ol>
+                                        <ol>
+                                            {store.product.IPhone}
+                                        </ol>
+                                    </ul>
+                                </div>
+                        }
+
+                    </div> {/* fin renderizado */}
+                </div>
+            </div >
         </div>
     )
 }
